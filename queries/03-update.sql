d910d4d2de2b7409409ac9b2465a580b6ba08140
Thales971 <thales.t.silva6@aluno.senai.br>
2025-09-22 14:44:51 -0300

    comandos iniciais de update

---
+++ queries/03-update.sql
@@ -0,0 +1,16 @@
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET username = 'ProGamer2025'
+Where username = 'NoobMaster';
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET pontos = pontos + 1000;
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET email = 'main@gmail.com'
+WHERE email = 'master@email.com';
+
+SELECT * FROM usuarios;
+
