5c14f2398fcd86100491a9b945a2bffdbeb233cd
Thales971 <thales.t.silva6@aluno.senai.br>
2025-09-29 11:45:31 -0300

    Cria testess

---
+++ testess/acentos.sql
@@ -0,0 +1,17 @@
+CREATE TABLE testess (
+id SERIAL PRIMARY KEY,
+nome VARCHAR(100) NOT NULL
+);
+
+INSERT INTO testess (nome)
+VALUES 
+('João'),
+('Stéfanny'),
+('ângelo'),
+('Márcia'),
+('Letícia'),
+('Luís'),
+('Cézar'),
+('Márcia'),
+('Renée'),
+('Óscar');
